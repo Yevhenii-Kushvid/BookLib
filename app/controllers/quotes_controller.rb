1046d0c6f79bc7a765c4fd34e2a2547250efc110
Mort Reany <pathfinder1994@mail.ru>
2016-02-05 16:48:44 +0200

    Changed way of checking rigts to quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,5 @@
 class QuotesController < ApplicationController
-  before_action :set_quote, only: [:show, :create, :edit, :update, :destroy]
-  before_action :is_creator?, only: [:edit, :update, :destroy]
+  #before_action :is_creator?, only: [:edit, :update, :destroy]
 
   def index
     @quotes = Book.find(params[:book_id]).quotes
@@ -11,78 +10,81 @@ class QuotesController < ApplicationController
   end
 
   def new
-    @quote = Quote.new
+    if book.user == current_user
+      @quote = Quote.new
+    else
+      render_500
+    end
   end
 
   def create
-    @quote = Quote.new(text: quote_params[:text])
-    @quote.user = current_user if current_user
-    @quote.book = @book if @book
+    book = Book.find(params[:book_id])
+    if book.user == current_user
+      @quote = Quote.new(text: quote_params[:text])
+      @quote.user = current_user if current_user
+      @quote.book = book if book
 
-    respond_to do |format|
-      if @quote.save
-        format.html { redirect_to book_path(@book), notice: 'Quote was successfully created.' }
-        format.json { render :show, status: :created, location: @book}
-      else
-        format.html { render :new }
-        format.json { render json: @quote.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @quote.save
+          format.html { redirect_to book_path(book), notice: 'Quote was successfully created.' }
+          format.json { render :show, status: :created, location: book}
+        else
+          format.html { render :new }
+          format.json { render json: @quote.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      render_500
     end
   end
 
   def edit
+    quote = Quote.find(params[:id])
+    if current_user == quote.user
+      @quote = quote
+    else
+      render_500
+    end
   end
 
   def update
+    @quote = Quote.find(quote_params[:id])
+    if current_user == @quote.user
+      book = Book.find(params[:book_id])
       respond_to do |format|
-        if @quote.update_attributes(quote_params)
-          format.html { redirect_to book_path(@book), notice: 'Quote was successfully updated.' }
-          format.json { render :show, status: :ok, location: @book }
+        if @quote.update_attributes(text: quote_params[:text])
+          format.html { redirect_to book_path(book), notice: 'Quote was successfully updated.' }
+          format.json { render :show, status: :ok, location: book }
         else
           format.html { render :edit }
           format.json { render json: @quote.errors, status: :unprocessable_entity }
         end
       end
+    else
+      render_500
+    end
   end
 
   def destroy
-    begin
-      @quote.destroy
-      respond_to do |format|
-        format.html { redirect_to book_path(@book), notice: 'Quote was successfully destroyed.' }
-        format.json { head :no_content }
+    @quote = Quote.find(params[:id])
+    if current_user == @quote.user
+      begin
+        book = Book.find(params[:book_id])
+        @quote.destroy
+        respond_to do |format|
+          format.html { redirect_to book_path(book), notice: 'Quote was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+      rescue => ex
+        render_500
       end
-    rescue => ex
+    else
       render_500
     end
   end
 
   private
-
   def quote_params
-    params.require(:quote).permit(:id, :text)
-  end
-
-  def set_quote
-    @book = Book.find(params[:book_id])
-    @quotes = @book.quotes
-    @quote = @quotes.find(params[:id])
-
-    render_500 unless @book
-  end
-
-  def is_creator(user)
-    self.user == user
-  end
-
-  def is_creator?
-    @quote = Quote.find(quote_params[:id]) unless @quote
-
-    if current_user == @quote.user
-      true
-    else
-      false
-      render_500
-    end
+    params[:quote]
   end
 end
