7772002121b2b29e3d00f8c364e7a53fdcb9f578
Kushvid Evhenii <pathfinder1994@mail.ru>
2016-02-22 12:22:01 +0200

    html replace ufter login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,8 @@ Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
 
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
   config.action_mailer.raise_delivery_errors = false
   config.active_support.deprecation = :log
