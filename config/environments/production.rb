e8e451426ca644e7954f1a04321d750c0b7f5795
Kushvid Evhenii <pathfinder1994@mail.ru>
2016-02-22 12:19:57 +0200

    html replace ufter login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,60 +1,42 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  config.cache_classes = true
+  config.eager_load = true
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
-
-  # Do not eager load code on boot.
-  config.eager_load = false
-
-  # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
-  # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-
-  # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
   config.assets.debug = true
-
-  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
-  # yet still be able to expire them through the digest params.
   config.assets.digest = true
-
-  # Adds additional error checking when serving assets at runtime.
-  # Checks for improperly declared sprockets dependencies.
-  # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
 
 #============================================================================================================================
 
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
+
+
   #config.cache_classes = true
+
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
+
+
   #config.eager_load = true
 
+
   # Full error reports are disabled and caching is turned on.
+
+
   #config.consider_all_requests_local       = false
   #config.action_controller.perform_caching = true
+
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
@@ -64,18 +46,27 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
+
+
   #config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
 
   # Compress JavaScripts and CSS.
   #config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+
+
   #config.assets.compile = true
+
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
+
+
   #config.assets.digest = true
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -88,7 +79,10 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
+
+
   #config.log_level = :debug
+
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -108,14 +102,27 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
+
+
   #config.i18n.fallbacks = true
 
+
   # Send deprecation notices to registered listeners.
+
+
   #config.active_support.deprecation = :notify
 
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
+
+
   #config.log_formatter = ::Logger::Formatter.new
 
+
   # Do not dump schema after migrations.
+
+
   #config.active_record.dump_schema_after_migration = false
+
+
 end
